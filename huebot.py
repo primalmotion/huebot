d3a67e49848d9a34fed6172c4aed7f9cd899d3bf
Antoine Mercadal <antoine.mercadal@gmail.com>
2014-04-19 19:52:59 -0700

    Various fixes, and turn lights on 20 minutes before sunset (which seems to be the trigger when my wife starts to whine about lights)

--- huebot.py
+++ huebot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2.7
 
 from datetime import datetime
+from datetime import timedelta
 import phue
 import astral
 import time
@@ -8,6 +9,9 @@ import json
 import commands
 from apscheduler.scheduler import Scheduler
 import config as _c
+
+
+
 
 def number_of_connected_devices(macs):
     ret = 0;
@@ -36,15 +40,18 @@ if __name__ == "__main__":
 
     def schedule_next_sunset():
         if _c.SUNSET_AUTO:
-            date = sunset_date(_c.SUNSET_CITY).replace(tzinfo=None)
+            date = sunset_date(_c.SUNSET_CITY).replace(tzinfo=None) - timedelta(minutes=20)
         else:
             date = datetime.now().replace(hour=_c.SUNSET_HOUR, minute=_c.SUNSET_MINUTES, second=0, tzinfo=None)
+
+        if date < datetime.now():
+            date = date + timedelta(days=1)
         scheduler.add_date_job(turn_lights_on, date)
 
     def turn_lights_on():
         scheduler.add_interval_job(check_devices_presence, seconds=_c.DEVICES_CHECK_INTERVAL)
         if number_of_connected_devices(_c.DEVICES_MACS) > 0:
-            _set_lights_state(bridge, True)
+           _set_lights_state(bridge, True)
 
     def turn_lights_off():
         scheduler.unschedule_func(check_devices_presence)
@@ -56,6 +63,7 @@ if __name__ == "__main__":
         global last_number_of_devices
 
         number_of_devices = number_of_connected_devices(_c.DEVICES_MACS)
+        print "FOUND %d DEVICES " % number_of_devices
 
         if number_of_devices == 0:
             _set_lights_state(bridge, False)
@@ -64,7 +72,6 @@ if __name__ == "__main__":
             _set_lights_state(bridge, True)
 
         last_number_of_devices = number_of_devices
-
 
     schedule_next_sunset()
     scheduler.add_cron_job(turn_lights_off, hour=_c.SLEEP_TIME_HOUR, minute=_c.SLEEP_TIME_MINUTES)
